04c2e04bddc873158325b54e66dbb1f22b650183
Matt McLaughlin <mamclaughlin87@gmail.com>
2018-02-22 23:07:08 -0600

    completed flask routes

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ time.sleep(1)
 Asylum = Base.classes.asylum
 BattleDeaths = Base.classes.battle_deaths
 Origin = Base.classes.origin
+CountryInfo = Base.classes.info_table
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -48,6 +49,78 @@ def names():
     country_names = [x[0] for x in country_names]
     
     return jsonify(country_names)
+
+@app.route("/data")
+def data():
+    asylum_data = session.query(Asylum).all()
+    origin_data = session.query(Origin).all()
+    battle_death_data = session.query(BattleDeaths).all()
+    country_objects = []
+
+    for i in range(0, len(asylum_data)):
+        country_object = {}
+        country_object['country_name'] = asylum_data[i].country_name
+        country_object['country_lat'] = asylum_data[i].country_lat
+        country_object['country_lon'] = asylum_data[i].country_lon
+        country_object['asylum_seekers'] = asylum_data[i].y2016
+        country_object['refugees'] = origin_data[i].y2016
+        country_object['battle_deaths'] = battle_death_data[i].y2016
+        country_objects.append(country_object)
+    
+    return jsonify(country_objects)
+
+@app.route("/country/<country>")
+def country_info(country):
+    country_data = session.query(CountryInfo).filter(CountryInfo.country_name == country).first()
+    asylum_data = session.query(Asylum).filter(Asylum.country_name == country).first()
+    origin_data = session.query(Origin).filter(Origin.country_name == country).first()
+    battle_death_data = session.query(BattleDeaths).filter(BattleDeaths.country_name == country).first()
+    def get_year_array(data):
+        output_array = []
+        output_array.append(data.y1990)
+        output_array.append(data.y1991)
+        output_array.append(data.y1992)
+        output_array.append(data.y1993)
+        output_array.append(data.y1994)
+        output_array.append(data.y1995)
+        output_array.append(data.y1996)
+        output_array.append(data.y1997)
+        output_array.append(data.y1998)
+        output_array.append(data.y1999)
+        output_array.append(data.y2000)
+        output_array.append(data.y2001)
+        output_array.append(data.y2002)
+        output_array.append(data.y2003)
+        output_array.append(data.y2004)
+        output_array.append(data.y2005)
+        output_array.append(data.y2006)
+        output_array.append(data.y2007)
+        output_array.append(data.y2008)
+        output_array.append(data.y2009)
+        output_array.append(data.y2010)
+        output_array.append(data.y2011)
+        output_array.append(data.y2012)
+        output_array.append(data.y2013)
+        output_array.append(data.y2014)
+        output_array.append(data.y2015)
+        output_array.append(data.y2016)
+
+        return output_array
+
+
+
+    country_object = {
+        'country_name':country_data.country_name,
+        'gdp': country_data.gdp_YR2015,
+        'population': country_data.population_YR2016,
+        'asylum_seekers': asylum_data.y2016,
+        'refugees': origin_data.y2016,
+        'battle_deaths': battle_death_data.y2016,
+        'refugee_years': get_year_array(origin_data),
+        'asylum_years': get_year_array(asylum_data),
+        'battle_years': get_year_array(battle_death_data),
+    }
+    return jsonify(country_object)
 
 if __name__ == "__main__":
     app.run(debug=True, port=9000)

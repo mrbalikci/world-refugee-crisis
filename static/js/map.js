0797532de092e7967f39eec7f984b7589d60a63f
Matt McLaughlin <mamclaughlin87@gmail.com>
2018-02-25 12:10:24 -0600

    adding map layers

--- static/js/map.js
+++ static/js/map.js
@@ -2,10 +2,11 @@ var URL = "/data"
 var mapboxKey = "pk.eyJ1IjoibW1jbGF1Z2hsaW44NyIsImEiOiJjamRoank1NjQwd2R1MzNybGppOG9kZTdsIn0.2JTZIjgBlzTvfKjs7Rw_Dg"
 var mapboxURL = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?"
 
-function createFeatures(data) {
-    console.log(URL);
-    console.log(data);
-    
+function createMap(data) {  
+    var asylum_markers = []
+    var refugee_markers = []
+    var death_markers = []
+
     for (i = 0; i < data.length; i++) {
         var coords = [data[i].country_lat, data[i].country_lon];
         var countryName = data[i].country_name;
@@ -16,40 +17,57 @@ function createFeatures(data) {
             radius: asylum_seekers/5,
             color: "green",
             fillColor: "green"
-        }).addTo(myMap);
+        });
         var refugee_marker = L.circle(coords, {
             radius: refugees/5,
             color: "yellow",
             fillColor: "yellow"
-        }).addTo(myMap);
+        });
         var death_marker = L.circle(coords, {
             radius: battle_deaths*20,
             color: "red",
             fillColor: "red"
-        }).addTo(myMap);
+        });
         var markerArray = [asylum_marker,refugee_marker,death_marker]
         for(j=0; j<markerArray.length; j++){
             markerArray[j].bindPopup(`<strong>${countryName}</strong><hr>
             Asylum Seekers: ${asylum_seekers}<br>
             Refugees: ${refugees}<br>
-            Battle Related Deaths: ${battle_deaths}`);
-            
+            Battle Related Deaths: ${battle_deaths}`); 
         };
+        asylum_markers.push(asylum_marker);
+        refugee_markers.push(refugee_marker);
+        death_markers.push(death_marker);
     };
+    var asylumLayer = L.layerGroup(asylum_markers);
+    var refugeeLayer = L.layerGroup(refugee_markers);
+    var deathLayer = L.layerGroup(death_markers);
+
+    var mapLayer = L.tileLayer(
+        mapboxURL + "access_token=" + mapboxKey
+    );
+
+    var baseMaps = {"Map":mapLayer}
+
+    var overlayMaps = {
+        "Asylum Seekers to Country": asylumLayer,
+        "Refugees from Country": refugeeLayer,
+        "Battle Related Deaths": deathLayer
+    };
+
+    var myMap = L.map("map", {
+        center: [20, -10],
+        zoom: 1.5,
+        layers: [mapLayer, deathLayer, refugeeLayer, asylumLayer]
+    });
+    
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false,
+        hideSingleBase: true
+    }).addTo(myMap);
 };
 
-// Creating map object
-var myMap = L.map("map", {
-    center: [20, -20],
-    zoom: 2.5
-});
-
-// Adding tile layer
-L.tileLayer(
-    mapboxURL + "access_token=" + mapboxKey
-).addTo(myMap);
-
+// import data and create map
 d3.json(URL, function (data) {
-    createFeatures(data)
+    createMap(data)
 });
-
